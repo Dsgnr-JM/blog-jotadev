1d23f9f08e3daebf839d6711b9236312f3de9277
jotadev <mejiajhoan2021@gmail.com>
2024-07-08 23:57:14 -0400

    Update seo and robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,16 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: "https://example.com",
+  integrations: [tailwind(), sitemap()],
+  site: "https://jotadv.vercel.app",
   markdown: {
     shikiConfig: {
       theme: "dracula",
-      wrap: true,
-    },
-  },
+      wrap: true
+    }
+  }
 });
